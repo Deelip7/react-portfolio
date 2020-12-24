f8fc683387c0115fee1d7c5d17cd6b109397b160
Theelipan prabakar <theelipan@gmail.com>
2020-12-24 16:58:56 -0500

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className='App'>
+      <Header />
+      <main></main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
