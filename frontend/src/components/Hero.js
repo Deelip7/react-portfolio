b1b90413e827ce37e3089b4a6d25834c1add27f0
Theelipan prabakar <theelipan@gmail.com>
2020-12-26 23:37:07 -0500

    Lottie hero image animation

--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import Lottie from 'lottie-web';
+import animate from 'lottie-web';
 
 const Hero = () => {
+  const container = useRef(null);
+
+  useEffect(() => {
+    Lottie.loadAnimation({
+      container: container.current,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData: require('./astronaout.json'),
+    });
+    animate.setSpeed(0.5);
+  }, []);
+
   return (
     <section className='my-16 px-5 md:my-32 flex flex-col flex-wrap space-y-16 md:space-y-0 patterns'>
       <div className='ml-auto w-72 md:w-96 lg:w-2/5'>
-        <img src='./images/Astronaut-01.svg' alt='Astronaut' />
+        <div className='container' ref={container}></div>
       </div>
       <div className='max-w-md mb-auto space-y-5'>
         <h1 className='text-5xl font-bold md:text-7xl'>Hello. I’m Theelipan</h1>
