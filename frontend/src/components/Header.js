9775e2cf3589dff5158d35351744406a13276b9e
Theelipan prabakar <theelipan@gmail.com>
2021-01-01 12:11:21 -0500

    fix header links

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,14 +4,18 @@ const Header = () => {
   return (
     <nav className='space-x-10'>
       <ul className='flex flex-row space-x-4 md:space-x-10 md:text-xl justify-center md:justify-end font-thin'>
-        <li className='py-1 px-5 hover:bg-gray focus:bg-gray rounded-lg'>
-          <a href='#projects'>Projects</a>
+        <li>
+          <a href='#projects' className='py-1 px-5 hover:bg-gray focus:bg-gray rounded-lg'>
+            Projects
+          </a>
         </li>
-        <li className='py-1 px-5 hover:bg-gray focus:bg-gray rounded-lg'>
-          <a href='#contact'>Contact</a>
+        <li>
+          <a href='#contact' className='py-1 px-5 hover:bg-gray focus:bg-gray rounded-lg'>
+            Contact
+          </a>
         </li>
-        <li className='py-1 px-5 hover:bg-gray focus:bg-gray rounded-lg'>
-          <a href='./Theelipan  Prabakar - Resume.pdf' target='_blank' rel='noreferrer'>
+        <li>
+          <a href='./Theelipan  Prabakar - Resume.pdf' target='_blank' rel='noreferrer' className='py-1 px-5 hover:bg-gray focus:bg-gray rounded-lg'>
             Resume
           </a>
         </li>
