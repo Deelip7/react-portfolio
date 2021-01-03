628ae40555ae8adee40ec66cd9b1a93d07e2f79a
Theelipan prabakar <theelipan@gmail.com>
2021-01-03 17:01:16 -0500

    fix typo

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -15,7 +15,7 @@ const Header = () => {
           </a>
         </li>
         <li>
-          <a href='./Theelipan  Prabakar - Resume.pdf' target='_blank' rel='noreferrer' className='py-1 px-5 hover:bg-gray focus:bg-gray rounded-lg'>
+          <a href='./Resume.pdf' target='_blank' rel='noreferrer' className='py-1 px-5 hover:bg-gray focus:bg-gray rounded-lg'>
             Resume
           </a>
         </li>
