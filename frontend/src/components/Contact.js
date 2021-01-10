124bdacfd583200a50608b333554cf1426698b01
Theelipan prabakar <theelipan@gmail.com>
2021-01-09 22:45:08 -0500

    Netlify’s Form Handling

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -48,7 +48,9 @@ const Contact = () => {
         </div>
 
         <div className='bg-gray px-5 py-10 md:py-8 sm:p-8 my-2 md:rounded-lg shadow-lg  justify-between w-full  md:w-6/12 '>
-          <form className='flex flex-col space-y-3 m-auto w-full' name='contact' method='POST' data-netlify='true' data-netlify-recaptcha='true'>
+          <form className='flex flex-col space-y-3 m-auto w-full' name='contact' method='post'>
+            <input type='hidden' name='form-name' value='contact' />
+
             <label for='name'>Name</label>
             <input type='text' name='name' id='name' className='gradient'></input>
             <label for='email'>Email</label>
